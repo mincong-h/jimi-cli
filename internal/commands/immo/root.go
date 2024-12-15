fb274b53c937401e7d14890fb24d642df8216fea
Mincong Huang <mhuang@becomposable.com>
2024-12-15 22:33:46 +0100

    Fix

--- internal/commands/immo/root.go
+++ internal/commands/immo/root.go
@@ -5,11 +5,11 @@ import "github.com/spf13/cobra"
 var ImmoCmd = &cobra.Command{
 	Use:   "immo",
 	Short: `Commands for a real-estate  project.`,
-	Run:   runImmo,
+	RunE:  runImmo,
 }
 
-func runImmo(cmd *cobra.Command, args []string) {
-	cmd.Help()
+func runImmo(cmd *cobra.Command, args []string) error {
+	return cmd.Help()
 }
 
 func init() {
