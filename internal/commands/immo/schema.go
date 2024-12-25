6688497421122bcdb956b8ad1cea6d0df889e0c2
Mincong Huang <mhuang@becomposable.com>
2024-12-25 15:09:02 +0100

    Add show-schema

--- internal/commands/immo/schema.go
+++ internal/commands/immo/schema.go
@@ -1,14 +1,21 @@
 package immo
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/invopop/jsonschema"
 	"github.com/spf13/cobra"
 )
 
 var showSchemaCmd = &cobra.Command{
-	Use:   "evaluate",
-	Short: "Evaluate different scenarios of a real-estate purchase.",
+	Use:   "show-schema",
+	Short: "Show the object schema for a real-estate offer in Vertesia",
 	Run:   runShowSchema,
 }
 
 func runShowSchema(cmd *cobra.Command, args []string) {
+	schema := jsonschema.Reflect(&Good{})
+	schemaJSON, _ := json.MarshalIndent(schema, "", "  ")
+	fmt.Println(string(schemaJSON))
 }
