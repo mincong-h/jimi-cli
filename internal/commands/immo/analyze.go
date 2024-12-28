c4cbab877e7c02edf6e1d8b656499874e334ea20
Mincong Huang <mhuang@becomposable.com>
2024-12-28 21:38:30 +0100

    Add analysis

--- internal/commands/immo/analyze.go
+++ internal/commands/immo/analyze.go
@@ -15,18 +15,30 @@ var analyzeCmd = &cobra.Command{
 	RunE:  runAnalyze,
 }
 
+var analysisInteractions = []string{
+	"mhuang-seloger:RenovationAnalysis",
+	"mhuang-seloger:LocationIntelligenceAnalysis",
+	"mhuang-seloger:LegalAndAdministrativeAnalysis",
+	"mhuang-seloger:MarketDynamicsAnalysis",
+	"mhuang-seloger:LifestyleAndFitAnalysis",
+	"mhuang-seloger:RisksAnalysis",
+}
+
 func runAnalyze(cmd *cobra.Command, args []string) error {
 	if len(args) != 1 {
 		return errors.New("Please provide a real-estate listing to analyze.")
 	}
 	property := args[0]
 	fmt.Printf("Analyzing real-estate listing %s...\n", property)
-	interaction := exec.Command("bash", "-c", "composable profiles")
-	interaction.Stdout = os.Stdout
-	interaction.Stderr = os.Stderr
-	if err := interaction.Run(); err != nil {
-		fmt.Printf("Failed to analyze: %v\n", err)
-		return fmt.Errorf("failed to analyze: %w", err)
+	for _, interaction := range analysisInteractions {
+		bashCmd := fmt.Sprintf(`composable run %s --data {"real_estate_listing": "store:%s"}`, interaction, property)
+		interaction := exec.Command("bash", "-c", bashCmd)
+		interaction.Stdout = os.Stdout
+		interaction.Stderr = os.Stderr
+		if err := interaction.Run(); err != nil {
+			fmt.Printf("Failed to analyze: %v\n", err)
+			return fmt.Errorf("failed to analyze: %w", err)
+		}
 	}
 	return nil
 }
